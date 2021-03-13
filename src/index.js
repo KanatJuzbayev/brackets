ca8ab5ef7a25023c3f25cfe95d44814833922a99
Kanat Juzbayev <76990478+KanatJuzbayev@users.noreply.github.com>
2021-03-13 23:50:00 +0600

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let single = [];
+  let arrBrackets = [];
+
+  bracketsConfig.forEach(i => {
+      i.forEach(y => {
+          arrBrackets.push(y)
+      })
+  });
+
+  let arrSameBrackets = [];
+
+  arrBrackets.forEach((i, index) => {
+      if (arrBrackets[index - 1] === i) {
+          arrSameBrackets.push(i)
+      }
+  });
+
+  if (str.length % 2 !== 0) return false;
+  for (let i = 0; i < str.length; i++) {
+      if (arrSameBrackets.includes(str[i])) {
+          if (str[i] === single[single.length - 1]) {
+              single.pop();
+          } else single.push(str[i]);
+      } else if (arrBrackets.indexOf(str[i]) % 2 === 1) {
+          if (single[single.length - 1] === arrBrackets[arrBrackets.indexOf(str[i]) - 1]) {
+              single.pop()
+          }
+      } else single.push(str[i])
+  }
+  return single.length === 0;
 }
